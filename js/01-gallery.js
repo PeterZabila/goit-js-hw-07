043ea0b4d6a3a21ec1c3207cd19036c1c3e52656
ZABILA PETRO <petrozabila@gmail.com>
2022-05-18 15:47:14 +0300

    HW 7 initil commit gallery 1 assignment

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+function addGalleryItems (galleryItems) {
+   const pictures = galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+             </a>
+        </div>`
+    }).join('');
+
+    gallery.insertAdjacentHTML('afterbegin', pictures);
+};
+
+addGalleryItems(galleryItems);
+
+gallery.addEventListener('click', onClickModal);
+
+function onClickModal(e) {
+    e.preventDefault();
+    if(e.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+        <img src="${e.target.dataset.source}">
+    `)
+
+    instance.show()
+
+    // new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt",});
+};
+
+
+
