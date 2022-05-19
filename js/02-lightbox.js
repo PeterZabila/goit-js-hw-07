5bb2dc814937e536ff6a5a5c8650dfbb17a878ba
Peter Zabila <petrozabila@gmail.com>
2022-05-20 00:35:53 +0300

    two gallery basis complete needs bugfix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function addGalleryItems (galleryItems) {
+   const pictures = galleryItems.map(({preview, original, description}) => {
+        return`
+        <li class="gallery__item">
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+
+
+    }).join('');
+
+    gallery.insertAdjacentHTML('afterbegin', pictures);
+};
+
+addGalleryItems(galleryItems);
+
+gallery.addEventListener('click', onClickModal);
+
+function onClickModal(e) {
+    e.preventDefault();
+    if(e.target.nodeName !== "IMG") {
+        return;
+    }
+
+   new SimpleLightbox('.gallery a', {  captionDelay: 250, captionsData: "alt",});
+
+};
